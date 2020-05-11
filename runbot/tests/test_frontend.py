e3418cece57c8f7edf62bc1e724fe6732e413d11
Xavier-Do <xdo@odoo.com>
2020-05-11 16:32:37 +0200

    remove front-end

--- runbot/tests/test_frontend.py
+++ runbot/tests/test_frontend.py
@@ -18,53 +18,52 @@ class Test_Frontend(RunbotCase):
             'remote_id': self.remote_server.id,
             'name': 'refs/heads/master',
             'sticky': True,
+            'is_pr': False
         })
         self.branch = self.Branch.create({
             'remote_id': self.remote_server.id,
             'name': 'refs/heads/master-test-moc',
             'sticky': False,
+            'is_pr': False
         })
         self.Build = self.env['runbot.build']
 
-    @patch('odoo.http.Response.set_default')
-    @patch('odoo.addons.runbot.controllers.frontend.request')
-    def test_frontend_basic(self, mock_request, mock_set_default):
-        mock_request.env = self.env
-        mock_request._cr = self.cr
-        controller = frontend.Runbot()
-
-        states = ['done', 'pending', 'testing', 'running']
-        branches = [self.branch, self.sticky_branch]
-        names = ['deadbeef', 'd0d0caca', 'deadface', 'cacafeed']
-        # create 5 builds in each branch
-        for i, state, branch, name in zip(range(8), cycle(states), cycle(branches), cycle(names)):
-            name = '%s%s' % (name, i)
-            build = self.Build.create({
-                'branch_id': branch.id,
-                'name': '%s0000ffffffffffffffffffffffffffff' % name,
-                'port': '1234',
-                'local_state': state,
-                'local_result': 'ok'
-                })
-
-        def mocked_simple_repo_render(template, context):
-            self.assertEqual(template, 'runbot.repo', 'The frontend controller should use "runbot.repo" template')
-            self.assertEqual(self.sticky_branch, context['branches'][0]['branch'], "The sticky branch should be in first place")
-            self.assertEqual(self.branch, context['branches'][1]['branch'], "The non sticky branch should be in second place")
-            self.assertEqual(len(context['branches'][0]['builds']), 4, "Only the 4 last builds should appear in the context")
-            self.assertEqual(context['pending_total'], 2, "There should be 2 pending builds")
-            self.assertEqual(context['pending_level'], 'info', "The pending level should be info")
-            return Response()
-
-        mock_request.render = mocked_simple_repo_render
-        controller.repo(repo=self.repo)
-
-        def mocked_repo_search_render(template, context):
-            dead_count = len([bu['name'] for b in context['branches'] for bu in b['builds'] if bu['name'].startswith('dead')])
-            undead_count = len([bu['name'] for b in context['branches'] for bu in b['builds'] if not bu['name'].startswith('dead')])
-            self.assertEqual(dead_count, 4, 'The search for "dead" should return 4 builds')
-            self.assertEqual(undead_count, 0, 'The search for "dead" should not return any build without "dead" in its name')
-            return Response()
-
-        mock_request.render = mocked_repo_search_render
-        controller.repo(repo=self.repo, search='dead')
+    #@patch('odoo.http.Response.set_default')
+    #@patch('odoo.addons.runbot.controllers.frontend.request')
+    #def test_frontend_basic(self, mock_request, mock_set_default):
+    #    mock_request.env = self.env
+    #    mock_request._cr = self.cr
+    #    controller = frontend.Runbot()
+#
+    #    states = ['done', 'pending', 'testing', 'running']
+    #    branches = [self.branch, self.sticky_branch]
+    #    names = ['deadbeef', 'd0d0caca', 'deadface', 'cacafeed']
+    #    # create 5 builds in each branch
+    #    for i, state, branch, name in zip(range(8), cycle(states), cycle(branches), cycle(names)):
+    #        name = '%s%s' % (name, i)
+    #        build = self.Build.create({
+    #            'local_state': state,
+    #            'local_result': 'ok'
+    #            })
+#
+    #    def mocked_simple_repo_render(template, context):
+    #        self.assertEqual(template, 'runbot.repo', 'The frontend controller should use "runbot.repo" template')
+    #        self.assertEqual(self.sticky_branch, context['branches'][0]['branch'], "The sticky branch should be in first place")
+    #        self.assertEqual(self.branch, context['branches'][1]['branch'], "The non sticky branch should be in second place")
+    #        self.assertEqual(len(context['branches'][0]['builds']), 4, "Only the 4 last builds should appear in the context")
+    #        self.assertEqual(context['pending_total'], 2, "There should be 2 pending builds")
+    #        self.assertEqual(context['pending_level'], 'info', "The pending level should be info")
+    #        return Response()
+#
+    #    mock_request.render = mocked_simple_repo_render
+    #    controller.repo(repo=self.repo)
+#
+    #    def mocked_repo_search_render(template, context):
+    #        dead_count = len([bu['name'] for b in context['branches'] for bu in b['builds'] if bu['name'].startswith('dead')])
+    #        undead_count = len([bu['name'] for b in context['branches'] for bu in b['builds'] if not bu['name'].startswith('dead')])
+    #        self.assertEqual(dead_count, 4, 'The search for "dead" should return 4 builds')
+    #        self.assertEqual(undead_count, 0, 'The search for "dead" should not return any build without "dead" in its name')
+    #        return Response()
+#
+    #    mock_request.render = mocked_repo_search_render
+    #    controller.repo(repo=self.repo, search='dead')
