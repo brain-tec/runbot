64997eb2ac95bd804aec15cba6a7414797fd0c36
Xavier-Do <xdo@odoo.com>
2020-06-03 16:17:42 +0200

    [IMP] runbot: runbot 5.0
    Runbot initial architechture was working for a single odoo repo, and was
    adapted to build enterprise. Addition of upgrade repo and test began
    to make result less intuitive revealing more weakness of the system.
    
    Adding to the oddities of duplicate detection and branch matching,
    there was some room for improvement in the runbot models.
    
    This (small) commit introduce the runbot v5.0, designed for a closer
    match of odoo's development flows, and hopefully improving devs
    experience and making runbot configuration more flexible.
    
    **Remotes:** remote intoduction helps to detect duplicate between odoo and
    odoo-dev repos: a commit is now on a repo, a repo having multiple remote.
    If a hash is in odoo-dev, we consider that it is the same in odoo.
    Note: github seems to manage commit kind of the same way. It is possible
    to send a status on a commit on odoo when the commit only exists in
    odoo-dev.
    This change also allows to remove some repo duplicate configuration
    between a repo and his dev corresponding repo.
    (modules, server files, manifests, ...)
    
    **Trigger:** before v5.0, only one build per repo was created, making it
    difficult to tweak what test to execute in what case. The example use
    case was for upgrade. We want to test upgrade to master when pushing on
    odoo. But we also want to test upgrade the same way when pushing on
    upgrade. We introduce a build that should be ran on pushing on either
    repo when each repo already have specific tests.
    The trigger allows to specify a build to create with a specific config.
    The trigger is executed when any repo of the trigger repo is pushed.
    The trigger can define depedencies: only build enterprise when pushing
    enterprise, but enterprise needs odoo. Test upgrade to master when pushing
    either odoo or upgrade.
    Trigger will also allows to extract some build like cla that where
    executed on both enterprise and odoo, and hidden in a subbuild.
    
    **Bundle:** Cross repo branches/pr branches matching was hidden in build
    creation and can be confusing. A build can be detected as a duplicate
    of a pr, but not always if naming is wrong or traget is invalid/changes.
    This was mainly because of how a community ref will be found. This was
    making ci on pr undeterministic if duplicate matching fails. This was
    also creating two build, with one pointing to the other when duplicate
    detection was working, but the visual result can be confusing.
    Associtaions of remotes and bundles fix this by adding all pr and
    related branches from all repo in a bundle. First of all this helps to
    visualise what the runbot consider has branch matching and that should
    be considered as part of the same task, giving a place where to warn
    devs of some possible inconsistencies. Associate whith repo/remote, we
    can consider branches in the same repo in a bundle as expected to have
    the same head. Only one build is created since trigger considers repo,
    not remotes.
    
    **Batch:** A batch is a group of build, a batch on a bundle can be
    compared to a build on a branch in previous version. When a branch
    is pushed, the corresponding bundle creates a new batch, and wait for
    new commit. Once no new update are detected in the batch for 60 seconds,
    All the trigger are executed if elligible. The created build are added
    to the batch in a batch_slot. It is also possible that an corresponding
    build exists (duplicate) and is added to the slot instead of creating a
    new build.
    
    Co-authored-by d-fence <moc@odoo.com>

---
+++ runbot/models/commit.py
@@ -0,0 +1,150 @@
+
+from ..common import os
+import glob
+from odoo import models, fields, api, registry
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class Commit(models.Model):
+    _name = 'runbot.commit'
+    _description = "Commit"
+
+    _sql_constraints = [
+        (
+            "commit_unique",
+            "unique (name, repo_id)",
+            "Commit must be unique to ensure correct duplicate matching",
+        )
+    ]
+    name = fields.Char('SHA')
+    repo_id = fields.Many2one('runbot.repo', string='Repo group')
+    date = fields.Datetime('Commit date')
+    author = fields.Char('Author')
+    author_email = fields.Char('Author Email')
+    committer = fields.Char('Committer')
+    committer_email = fields.Char('Committer Email')
+    subject = fields.Text('Subject')
+    dname = fields.Char('Display name', compute='_compute_dname')
+
+    def _get_available_modules(self):
+        for manifest_file_name in self.repo_id.manifest_files.split(','):  # '__manifest__.py' '__openerp__.py'
+            for addons_path in (self.repo_id.addons_paths or '').split(','):  # '' 'addons' 'odoo/addons'
+                sep = os.path.join(addons_path, '*')
+                for manifest_path in glob.glob(self._source_path(sep, manifest_file_name)):
+                    module = os.path.basename(os.path.dirname(manifest_path))
+                    yield (addons_path, module, manifest_file_name)
+
+    def export(self):
+        return self.repo_id._git_export(self.name)
+
+    def read_source(self, file, mode='r'):
+        file_path = self._source_path(file)
+        try:
+            with open(file_path, mode) as f:
+                return f.read()
+        except:
+            return False
+
+    def _source_path(self, *path):
+        return self.repo_id._source_path(self.name, *path)
+
+    @api.depends('name', 'repo_id.name')
+    def _compute_dname(self):
+        for commit in self:
+            commit.dname = '%s:%s' % (commit.repo_id.name, commit.name[:8])
+
+    def _github_status(self, build, context, state, target_url, description=None, post_commit=True):
+        self.ensure_one()
+        Status = self.env['runbot.commit.status']
+        last_status = Status.search([('commit_id', '=', self.id), ('context', '=', context)], order='id desc', limit=1)
+        if last_status and last_status.state == state:
+            _logger.info('Skipping already sent status %s:%s for %s', context, state, self.name)
+            return
+        last_status = Status.create({
+            'build_id': build.id if build else False,
+            'commit_id': self.id,
+            'context': context,
+            'state': state,
+            'target_url': target_url,
+            'description': description or context,
+        })
+        last_status._send(post_commit)
+
+
+class CommitLink(models.Model):
+    _name = 'runbot.commit.link'
+    _description = "Build commit"
+
+    commit_id = fields.Many2one('runbot.commit', 'Commit', required=True, index=True)
+    # Link info
+    match_type = fields.Selection([('new', 'New head of branch'), ('head', 'Head of branch'), ('base_head', 'Found on base branch'), ('base_match', 'Found on base branch')])  # HEAD, DEFAULT
+    branch_id = fields.Many2one('runbot.branch', string='Found in branch')  # Shouldn't be use for anything else than display
+
+    base_commit_id = fields.Many2one('runbot.commit', 'Base head commit', index=True)
+    merge_base_commit_id = fields.Many2one('runbot.commit', 'Merge Base commit', index=True)
+    base_behind = fields.Integer('# commits behind base')
+    base_ahead = fields.Integer('# commits ahead base')
+    file_changed = fields.Integer('# file changed')
+    diff_add = fields.Integer('# line added')
+    diff_remove = fields.Integer('# line removed')
+
+
+class CommitStatus(models.Model):
+    _name = 'runbot.commit.status'
+    _description = 'Commit status'
+    _order = 'id desc'
+
+    commit_id = fields.Many2one('runbot.commit', string='Commit', required=True, index=True)
+    context = fields.Char('Context', required=True)
+    state = fields.Char('State', required=True)
+    build_id = fields.Many2one('runbot.build', string='Build', index=True)
+    target_url = fields.Char('Url')
+    description = fields.Char('Description')
+    sent_date = fields.Datetime('Sent Date')
+
+    def _send(self, post_commit=True):
+        user_id = self.env.user.id
+        _dbname = self.env.cr.dbname
+        _context = self.env.context
+
+        status_id = self.id
+        commit = self.commit_id
+        all_remote = commit.repo_id.remote_ids
+        remotes = all_remote.filtered(lambda remote: remote.token)
+        no_token_remote = all_remote-remotes
+        if no_token_remote:
+            _logger.warning('No token on remote %s, skipping status', no_token_remote.mapped("name"))
+        remote_ids = remotes.ids
+        commit_name = commit.name
+
+        status = {
+            'context': self.context,
+            'state': self.state,
+            'target_url': self.target_url,
+            'description': self.description,
+        }
+        if remote_ids:
+
+            def send_github_status(env):
+                for remote in env['runbot.remote'].browse(remote_ids):
+                    _logger.debug(
+                        "github updating %s status %s to %s in repo %s",
+                        status['context'], commit_name, status['state'], remote.name)
+                    remote._github('/repos/:owner/:repo/statuses/%s' % commit_name, status, ignore_errors=True)
+                    env['runbot.commit.status'].browse(status_id).sent_date = fields.Datetime.now()
+
+            def send_github_status_async():
+                try:
+                    db_registry = registry(_dbname)
+                    with api.Environment.manage(), db_registry.cursor() as cr:
+                        env = api.Environment(cr, user_id, _context)
+                        send_github_status(env)
+                except:
+                    _logger.exception('Something went wrong sending notification for %s', commit_name)
+
+            if post_commit:
+                self._cr.after('commit', send_github_status_async)
+            else:
+                send_github_status(self.env)
