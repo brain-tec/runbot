7d9f6f3aa1da10d1b5e2c34d3365dfdf965a30e8
Christophe Monniez <moc@odoo.com>
2020-11-05 11:55:44 +0100

    update dockerfile tests

--- runbot/tests/test_dockerfile.py
+++ runbot/tests/test_dockerfile.py
@@ -9,11 +9,15 @@ _logger = logging.getLogger(__name__)
 class TestDockerfile(RunbotCase):
 
     def test_dockerfile_base_fields(self):
-        xml_content = """
-        <t t-name="runbot.docker_base">
-        <t t-set="docker_from" t-value="docker_from or 'ubuntu:focal'"/>
-        <t t-call="runbot.docker_from"/>
-        </t>"""
+        xml_content = """<t t-call="runbot.docker_base">
+    <t t-set="custom_values" t-value="{
+      'from': 'ubuntu:focal',
+      'phantom': True,
+      'additional_pip': 'babel==2.8.0',
+      'chrome_version': '86.0.4240.183-1',
+    }"/>
+</t>
+"""
 
         focal_template = self.env['ir.ui.view'].create({
             'name': 'docker_focal_test',
@@ -23,10 +27,13 @@ class TestDockerfile(RunbotCase):
         })
 
         dockerfile = self.env['runbot.dockerfile'].create({
-            'name': 'Ubuntu Focal (20.0)[Chrome 80]',
+            'name': 'Ubuntu Focal (20.0)[Chrome 86]',
             'template_id': focal_template.id,
             'to_build': False
         })
 
-        self.assertEqual(dockerfile.image_tag, 'odoo:UbuntuFocal20.0Chrome80')
+        self.assertEqual(dockerfile.image_tag, 'odoo:UbuntuFocal20.0Chrome86')
         self.assertTrue(dockerfile.dockerfile.startswith('FROM ubuntu:focal'))
+        self.assertIn(' apt-get install -y -qq google-chrome-stable=86.0.4240.183-1', dockerfile.dockerfile)
+        self.assertIn('# Install phantomjs', dockerfile.dockerfile)
+        self.assertIn('pip install babel==2.8.0', dockerfile.dockerfile)
