47cee4854b282ff1de27fef2e856770d2d58cd2b
Xavier-Do <xdo@odoo.com>
2021-12-08 13:50:23 +0100

    [IMP] runbot: add populated data

--- runbot/models/project.py
+++ runbot/models/project.py
@@ -10,6 +10,7 @@ class Project(models.Model):
 
     trigger_ids = fields.One2many('runbot.trigger', 'project_id', string='Triggers')
     dockerfile_id = fields.Many2one('runbot.dockerfile', index=True, help="Project Default Dockerfile")
+    repo_ids = fields.One2many('runbot.repo', 'project_id', string='Repos')
 
 
 class Category(models.Model):
