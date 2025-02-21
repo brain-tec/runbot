fb7b8480b85422503bd120b41eb7b9e390a034df
Xavier-Do <xdo@odoo.com>
2025-02-21 09:56:34 +0100

    [IMP] runbot: introduce matrix for upgrade
    The initial way to configure upgrades was working fine and automatically
    but does not alway match the reality of what should be supported.
    
    Saying, upgrade from the last main version and last intermediate
    (for 18, from 17.0 and 17.4) we may also want to test from 17.2 because
    for some reason there are many people in 17.2 taht will need to upgrade
    in 18.0. But the transition 17.2->17.3 doesn't make sense anymore
    after a while since people will go in 18 imediatly at some point.
    
    This matrix can be generated automatically from the parameters and new
    versions should appear automatically, but it is possible to tweak the
    values on demand.

--- runbot/static/src/js/fields/fields.js
+++ runbot/static/src/js/fields/fields.js
@@ -10,11 +10,17 @@ import { useDynamicPlaceholder } from "@web/views/fields/dynamic_placeholder_hoo
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { useInputField } from "@web/views/fields/input_field_hook";
 
-import { useRef, xml, Component } from "@odoo/owl";
+import { useRef, xml, Component, onWillRender } from "@odoo/owl";
 import { useAutoresize } from "@web/core/utils/autoresize";
 import { getFormattedValue } from "@web/views/utils";
 
 import { UrlField } from "@web/views/fields/url/url_field";
+
+import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
+import { formatX2many } from "@web/views/fields/formatters";
+
+import { BooleanToggleField } from "@web/views/fields/boolean_toggle/boolean_toggle_field";
+
 
 function stringify(obj) {
     return JSON.stringify(obj, null, '\t')
@@ -170,3 +176,54 @@ registry.category("fields").add("pull_request_url", {
 //}
 //
 //registry.category("fields").add("github_team", GithubTeamWidget);
+
+
+
+
+export class Matrixx2ManyField extends X2ManyField {
+    static template = 'runbot.Matrixx2ManyField';
+    static props = { ...standardFieldProps };
+
+    static components = { BooleanToggleField };
+
+    setup() {
+        
+        onWillRender(() => {
+            this.initValues();
+        })
+    }
+
+    getEntry(from, to) {
+        const entry = this.entry_per_version[[from, to]];
+        return entry
+    }
+
+    initValues() {
+        this.data = this.props.record.data[this.props.name]
+        this.to_versions = [];
+        this.from_versions = [];
+        this.entry_per_version = {};
+        this.data.records.forEach((record) => {
+            if (!this.to_versions.includes(record.data.to_version_number)) {
+                this.to_versions.push(record.data.to_version_number)
+            }
+            if (!this.from_versions.includes(record.data.from_version_number)) {
+                this.from_versions.push(record.data.from_version_number)
+            }
+            this.entry_per_version[[record.data.from_version_number, record.data.to_version_number]] = record;
+        });
+        console.log(this.to_versions)
+        this.to_versions.sort()
+        //this.to_versions.reverse()
+        this.from_versions.sort()
+        this.from_versions.reverse()
+    }
+}
+
+export const matrixx2ManyField = {
+    component: Matrixx2ManyField,
+    useSubView: false,
+};
+
+
+registry.category("fields").add("version_matrix", matrixx2ManyField);
