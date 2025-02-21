c5d3c85d6a3ce29c5212af0473c8267c7766afcf
Xavier-Do <xdo@odoo.com>
2025-02-21 09:56:34 +0100

    [IMP] runbot: introduce matrix for upgrade
    The initial way to configure upgrades was working fine and automatically
    but does not alway match the reality of what should be supported.
    
    Saying, upgrade from the last main version and last intermediate
    (for 18, from 17.0 and 17.4) we may also want to test from 17.2 because
    for some reason there are many people in 17.2 taht will need to upgrade
    in 18.0. But the transition 17.2->17.3 doesn't make sense anymore
    after a while since people will go in 18 imediatly at some point.
    
    This matrix can be generated automatically from the parameters and new
    versions should appear automatically, but it is possible to tweak the
    values on demand.

--- runbot/models/bundle.py
+++ runbot/models/bundle.py
@@ -216,6 +216,9 @@ class Bundle(models.Model):
             if records.is_base:
                 model = self.browse()
                 model.env.registry.clear_cache()
+                matrix = self.env['runbot.upgrade.matrix'].search([('project_id', '=', record.project_id.id)], limit=1)
+                if matrix:
+                    matrix._update_matrix_entries()
             elif record.project_id.tmp_prefix and record.name.startswith(record.project_id.tmp_prefix):
                 record['no_build'] = True
             elif record.project_id.staging_prefix and record.name.startswith(record.project_id.staging_prefix):
