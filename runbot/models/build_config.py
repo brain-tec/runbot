27c84f7adf0736f7914ce51cb6b0e595706ed7cb
Xavier-Do <xdo@odoo.com>
2026-01-05 15:10:53 +0100

    [IMP] runbot: allow to define an addon path when filtering modules
    Mainly for industry, the modules are not in an addons path but in
    another folder that should not be added as an addons path when starting
    odoo.
    
    This will allow to get this modules list.
    
    This may also allow to install only modules in a specific folder.

--- runbot/models/build_config.py
+++ runbot/models/build_config.py
@@ -46,6 +46,12 @@ def filter_all_modules(selector, build, dynamic_vars):
     return filter_default_modules(selector, build, dynamic_vars)
 
 
+def filter_addons_paths(selector, build, dynamic_vars, addons_paths):
+    build._checkout()  # we need to ensure source are exported before _get_modules_to_test
+    modules = build._get_modules_to_test(selector, addons_paths=addons_paths)
+    return ','.join(modules)
+
+
 def filter_default_modules(selector, build, dynamic_vars):
     build._checkout()  # we need to ensure source are exported before _get_modules_to_test
     modules = build._get_modules_to_test(selector)
@@ -1761,6 +1767,7 @@ class ConfigStep(models.Model):
 
         expression_filters = {
             'filter_all_modules': filter_all_modules,
+            'filter_addons_paths': filter_addons_paths,
             'filter_default_modules': filter_default_modules,
             'make_module_test_tags': make_module_test_tags,
             'prepend': prepend_string,
